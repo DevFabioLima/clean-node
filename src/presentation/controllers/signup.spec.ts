138229fb6868c71dd968192b8e9f8550cb5112e3
Fabio Lima <fabio.lucaslima@outlook.com>
2022-04-16 07:56:50 -0300

    chore: add old version husky

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import SignUpController from './signup';
 
-describe('SignUp Controller ', () => {
+describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController();
     const httRequest = {
