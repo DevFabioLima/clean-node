d484956d80b59a4db7ad3e2eee8093a675362d24
Fabio Lima <fabio.lucaslima@outlook.com>
2022-04-27 21:00:20 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/db/mongodb/account-repository/account.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { AddAccountRepository } from '../../../data/protocols/add-account-repository';
+import { AccountModel } from '../../../domain/models/account';
+import { AddAccountModel } from '../../../domain/usecases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const { insertedId: id } = result;
+    const accountById = await accountCollection.findOne({ _id: id });
+    const { _id, ...accountWithoutId } = accountById;
+
+    const response = ({
+      ...accountWithoutId,
+      id: _id.toHexString(),
+    }) as AccountModel;
+
+    return response;
+  }
+}
