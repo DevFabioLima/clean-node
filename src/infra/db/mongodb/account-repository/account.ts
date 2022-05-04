44de4b0afe7d8d681719e338a1e56a5f6f796b1a
Fabio Lima <fabio.lucaslima@outlook.com>
2022-05-03 21:26:48 -0300

    refactor: folder db inside infra and connect to mongodb before integration tests

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { AddAccountRepository } from '../../../data/protocols/add-account-repository';
-import { AccountModel } from '../../../domain/models/account';
-import { AddAccountModel } from '../../../domain/usecases/add-account';
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
