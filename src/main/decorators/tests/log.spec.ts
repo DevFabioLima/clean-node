65a34b06a8dff433d895a1cfe58d0a03a225e0da
Fabio Lima <fabio.lucaslima@outlook.com>
2022-05-08 10:14:53 -0300

    refactor: create tests folder

--- src/main/decorators/tests/log.spec.ts
+++ src/main/decorators/tests/log.spec.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-promise-executor-return */
-import { LogErrorRepository } from '../../data/protocols/log-error-repository';
-import { serverError } from '../../presentation/helpers/http-helper';
-import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
-import { LogControllerDecorator } from './log';
+import { LogErrorRepository } from '../../../data/protocols/log-error-repository';
+import { serverError } from '../../../presentation/helpers/http-helper';
+import { Controller, HttpRequest, HttpResponse } from '../../../presentation/protocols';
+import { LogControllerDecorator } from '../log';
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
